bf2141fffe3a21c8fe61f824820d8543e229266c
Fernando Larrañaga <xabadu@gmail.com>
2017-05-08 20:42:45 -0700

    Add searchTerm key to objects

--- blassie.js
+++ blassie.js
@@ -12,11 +12,13 @@ images.forEach(function(image) {
   const tempObj = {};
   const cleanKey = image.slice(0, -4);
   const slug = createSlug(cleanKey);
+  const searchTerm = cleanKey.split('-').join(' ');
   let imageFile = fs.readFileSync(IMAGE_PATH + '/' + image);
   imageFile = new Buffer(imageFile, 'binary').toString('base64');
   tempObj[slug] = {
     key: slug,
-    name: _.titleize(cleanKey.split('-').join(' ')),
+    searchTerm: searchTerm,
+    name: _.titleize(searchTerm),
     image: 'data:image/jpg;base64,' + imageFile
   }
   _.extend(baseStructure.wrestlers, tempObj);
